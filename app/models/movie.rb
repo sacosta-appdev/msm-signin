2ebe5fcd9dd4b8a3f7cc070b91bf5d85a5d13d18
Santiago Acosta <sacostal@chicagobooth.edu>
2023-05-09 14:50:31 +0000

    Added Associations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,4 +13,10 @@
 #  director_id :integer
 #
 class Movie < ApplicationRecord
+
+  belongs_to :director
+  has_many :characters
+
+  has_many :cast, :through => :characters, :source => :actor
+
 end
